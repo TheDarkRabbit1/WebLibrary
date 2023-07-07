db3b5b4dfec617da3ce1fc289956f599a5bef6f0
TDR <darkrabbitthe@gmail.com>
2023-07-08 00:17:02 +0300

    init

---
+++ src/main/java/com/example/weblibrary/data/book/Book.java
@@ -0,0 +1,15 @@
+package com.example.weblibrary.data.book;
+
+import com.example.weblibrary.data.BookCategory;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Book {
+    private Long id;
+    private String name;
+    private String author;
+    private String description;
+    private BookCategory bookCategory;
+}
