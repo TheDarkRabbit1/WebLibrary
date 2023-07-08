8bf738691bcbbabec264fad0d965a38c840f0e7f
TDR <darkrabbitthe@gmail.com>
2023-07-08 13:25:19 +0300

    cruds

---
+++ src/main/java/com/example/weblibrary/utils/SqlQueries.java
@@ -0,0 +1,18 @@
+package com.example.weblibrary.utils;
+
+public class SqlQueries {
+    public final static String getBookById = "SELECT * FROM book JOIN book_category on book_category.id = book.bookcategory_id WHERE book.id=?";
+    public final static String getBooks = "SELECT * FROM book JOIN book_category on book_category.id = book.bookcategory_id";
+    public final static String insertBook = "INSERT INTO book (name, author, description, bookCategory_id) VALUES (?, ?, ?, ?)";
+    public final static String getBooksByCategoryId = "SELECT * from book JOIN book_category on book_category.id = book.bookcategory_id WHERE book.bookcategory_id=?";
+    public final static String getBooksByCategoryName = "SELECT * from book JOIN book_category on book_category.id = book.bookcategory_id WHERE book_category.name=?";
+    public final static String deleteBookById = "DELETE FROM book where book.id=?";
+    public final static String deleteBooks = "DELETE FROM book";
+
+
+    public final static String getBookCategoryById="select * from book_category where id=?";
+    public final static String getBookCategories="select * from book_category";
+    public final static String insertBookCategory="insert into book_category (name) Values(?)";
+    public final static String deleteBookCategoryById="delete from book_category where book_category.id=?";
+    public final static String deleteBookCategories="delete from book_category";
+}
