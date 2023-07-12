b02c6def4c2d165e4f2599bffc9bd72e5f6e90f2
TDR <darkrabbitthe@gmail.com>
2023-07-12 23:40:03 +0300

    added security with chainfilter

--- src/main/java/com/example/weblibrary/config/WebSecurityConfig.java
+++ src/main/java/com/example/weblibrary/config/WebSecurityConfig.java
@@ -1,19 +1,47 @@
 package com.example.weblibrary.config;
 
+import com.example.weblibrary.data.user.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.DefaultLoginPageConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class WebSecurityConfig {
 
+    private CustomUserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorize -> authorize.anyRequest().permitAll());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/images/**", "/styles/**").permitAll()
+                .requestMatchers("/books", "/").permitAll()
+                .requestMatchers("/books/add", "/books/delete", "/books/bookForm").hasAuthority("LIBRARIAN")
+                .requestMatchers("/registration", "/registerNewUser").permitAll()
+                .anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+        http.apply(new DefaultLoginPageConfigurer<>());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 }
