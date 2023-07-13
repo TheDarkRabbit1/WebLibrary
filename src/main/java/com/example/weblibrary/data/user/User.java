e0bf4d69f017526c749156be039232cd4a5f71bf
TDR <darkrabbitthe@gmail.com>
2023-07-13 11:46:16 +0300

    added validation for form registration

--- src/main/java/com/example/weblibrary/data/user/User.java
+++ src/main/java/com/example/weblibrary/data/user/User.java
@@ -2,6 +2,8 @@ package com.example.weblibrary.data.user;
 
 import com.example.weblibrary.data.user.userRoles.UserRoles;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,7 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
     private Long id;
+    @NotNull
+    @Size(min = 3, max = 20, message = "should be from 3 to 20 symbols")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "should contain only letters and digits")
     private String username;
+    @NotNull
+    @Size(min = 4, message = "should be bigger then 4 characters")
+    @Pattern(regexp = "^(?=.*[0-9a-zA-Z]).{4,}$", message = "must contain at least one digit or letter")
     private String password;
     @NotNull
     private UserRoles role;
